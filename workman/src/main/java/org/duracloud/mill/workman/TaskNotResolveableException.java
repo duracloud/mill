4a9bea01f4a50252e0e7f3d95bc20b27535b79de
Daniel Bernstein <dabylon@gmail.com>
2013-10-16 11:51:40 -0700

    initial setup for workman subproject.

---
+++ workman/src/main/java/org/duracloud/mill/workman/TaskNotResolveableException.java
@@ -0,0 +1,18 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.mill.workman;
+
+
+/**
+ * @author Daniel Bernstein
+ */
+public class TaskNotResolveableException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+}
---
+++ workman/src/main/java/org/duracloud/mill/workman/TaskWorker.java
@@ -0,0 +1,17 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.mill.workman;
+/**
+ * It is responsible for preparing for, executing, and cleaning up after
+ * the <code>TaskProcessor</code>. 
+ * @author Daniel Bernstein
+ *
+ */
+public interface TaskWorker extends Runnable{
+
+}
