ca75749623cab57c39b630999174802ec5b2f730
Daniel Bernstein <dabylon@gmail.com>
2013-10-23 10:23:36 -0700

    Adds license boilerplate and documentation to DuplicationConfig.java

--- workman/src/main/java/org/duracloud/mill/dup/spring/DuplicationConfig.java
+++ workman/src/main/java/org/duracloud/mill/dup/spring/DuplicationConfig.java
@@ -1,3 +1,10 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
 package org.duracloud.mill.dup.spring;
 
 import org.duracloud.mill.credential.CredentialRepo;
@@ -6,6 +13,13 @@ import org.duracloud.mill.workman.RootTaskProcessorFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * A spring configuration that automatically hooks itself into the task
+ * processing framework.
+ * 
+ * @author Daniel Bernstein
+ * 
+ */
 @Configuration
 public class DuplicationConfig {
     @Bean
