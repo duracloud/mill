1cde7b622eef4219249eec2d78b857b12dc18520
Bill Branan <bbranan@duraspace.org>
2013-10-23 22:36:58 -0400

    Further updates to the duplication task processor.

---
+++ common/src/main/java/org/duracloud/mill/util/Retrier.java
@@ -0,0 +1,75 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.mill.util;
+
+import org.duracloud.common.util.WaitUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Handles performing actions that need to be retried.
+ *
+ * Expected usage example:
+ *
+ * Retrier retrier = new Retrier();
+ * return retrier.execute(new Retriable() {
+ *     @Override
+ *     public String retry() throws Exception {
+ *         // The actual method being executed
+ *         return doWork();
+ *     }
+ * });
+ *
+ * @author Bill Branan
+ *         Date: 10/23/13
+ */
+public class Retrier {
+
+    /**
+     * Default max number of attempts to make in performing a Retriable action
+     */
+    public static final int DEFAULT_MAX_RETRIES = 3;
+
+    private int maxRetries;
+
+    private final Logger log = LoggerFactory.getLogger(Retrier.class);
+
+    public Retrier() {
+        this.maxRetries = DEFAULT_MAX_RETRIES;
+    }
+
+    public Retrier(int maxRetries) {
+        this.maxRetries = maxRetries;
+    }
+
+    /**
+     * Executes the action of a Retriable, and retries on error. Provides a
+     * way to execute a variety of methods and allow a retry to occur on
+     * method failure.
+     *
+     * This method, along with the Retriable interface is an implementation
+     * of the command pattern.
+     *
+     * @param retriable
+     * @throws Exception
+     */
+    public <T extends Object> T execute(Retriable retriable) throws Exception {
+        Exception lastException = null;
+        for(int i=0; i<=maxRetries; i++) {
+            try {
+                return (T)retriable.retry();
+            } catch (Exception e) {
+                lastException = e;
+                log.debug(e.getMessage(), e);
+                WaitUtil.wait(i);
+            }
+        }
+        throw lastException;
+    }
+
+}
--- workman/src/main/java/org/duracloud/mill/dup/DuplicationTaskProcessor.java
+++ workman/src/main/java/org/duracloud/mill/dup/DuplicationTaskProcessor.java
@@ -8,6 +8,8 @@
 package org.duracloud.mill.dup;
 
 import org.duracloud.mill.domain.DuplicationTask;
+import org.duracloud.mill.util.Retriable;
+import org.duracloud.mill.util.Retrier;
 import org.duracloud.mill.workman.TaskExecutionFailedException;
 import org.duracloud.mill.workman.TaskProcessor;
 import org.duracloud.storage.error.NotFoundException;
@@ -41,49 +43,38 @@ public class DuplicationTaskProcessor implements TaskProcessor {
         String spaceId = task.getSpaceId();
         String contentId = task.getContentId();
 
-        // Retrieve metadata for content item from both providers
-        Map<String, String> sourceMetadata = null;
-        try { // TODO: Add retry
-            sourceMetadata =
-                sourceStore.getContentProperties(spaceId, contentId);
-        } catch(NotFoundException nfe) {
-            sourceMetadata = null;
-        }
+        // Retrieve properties for content items from both providers
+        Map<String, String> sourceProperties =
+            retryGetContentProperties(sourceStore, spaceId, contentId);
+        Map<String, String> destProperties =
+            retryGetContentProperties(destStore, spaceId, contentId);
 
-        Map<String, String> destMetadata = null;
-        try { // TODO: Add retry
-            destMetadata =
-                destStore.getContentProperties(spaceId, contentId);
-        } catch(NotFoundException nfe) {
-            destMetadata = null;
-        }
-
-        if(null != sourceMetadata) { // Item exists in source provider
-            if(null != destMetadata) { // Item exists in dest provider
+        if(null != sourceProperties) { // Item exists in source provider
+            if(null != destProperties) { // Item exists in dest provider
                 // Item exists in both providers, compare checksums
-                String srcChecksum = sourceMetadata.get(
+                String srcChecksum = sourceProperties.get(
                     StorageProvider.PROPERTIES_CONTENT_CHECKSUM);
-                String destChecksum = destMetadata.get(
+                String destChecksum = destProperties.get(
                     StorageProvider.PROPERTIES_CONTENT_CHECKSUM);
 
                 if(null != srcChecksum) {
                     if(srcChecksum.equals(destChecksum)) {
-                        // Source and destination are equal, verify metadata
-                        boolean metadataEqual =
-                            compareMetadata(sourceMetadata, destMetadata);
-                        if(!metadataEqual) {
-                            // Metadata is not equal, duplicate the metadata
-                            duplicateMetadata(spaceId,
-                                              contentId,
-                                              sourceMetadata);
+                        // Source and destination checksums are equal
+                        // Check to see if content properties are consistent
+                        boolean propertiesEqual =
+                            compareProperties(sourceProperties, destProperties);
+                        if(!propertiesEqual) {
+                            // Properties are not equal, duplicate the props
+                            duplicateProperties(spaceId, contentId,
+                                                sourceProperties);
                         }
                     } else {
                         // Source and destination content is not equal, duplicate
                         duplicateContent(spaceId, contentId);
                     }
                 } else {
-                    // Source item metadata has no checksum!
-                    failTask("Source content item metadata " +
+                    // Source item properties has no checksum!
+                    failTask("Source content item properties " +
                              "included no checksum!");
                 }
             } else { // Item in source but not in destination, duplicate
@@ -95,29 +86,49 @@ public class DuplicationTaskProcessor implements TaskProcessor {
         }
     }
 
+    protected Map<String, String> retryGetContentProperties(final StorageProvider store,
+                                                            final String spaceId,
+                                                            final String contentId)
+        throws TaskExecutionFailedException {
+        try {
+            return new Retrier().execute(new Retriable() {
+                @Override
+                public Map<String, String> retry() throws Exception {
+                    // The actual method being executed
+                    return store.getContentProperties(spaceId, contentId);
+                }
+            });
+        } catch(NotFoundException nfe) {
+            return null;
+        } catch(Exception e) {
+            failTask("Error attempting to retrieve content properties: " +
+                     e.getMessage(), e);
+        }
+        return null; // Should never actually be executed
+    }
+
     /**
-     * Determines if source and destination metadata are consistent
+     * Determines if source and destination properties are consistent
      *
-     * @param sourceMetadata
-     * @param destMetadata
+     * @param sourceProps
+     * @param destProps
      * @return
      */
-    protected boolean compareMetadata(Map<String, String> sourceMetadata,
-                                      Map<String, String> destMetadata) {
+    protected boolean compareProperties(Map<String, String> sourceProps,
+                                        Map<String, String> destProps) {
         // TODO implement
         return true;
     }
 
     /**
-     * Copies the metadata from the source item to the destination item.
+     * Copies the properties from the source item to the destination item.
      *
      * @param spaceId
      * @param contentId
-     * @param sourceMetadata
+     * @param sourceProperties
      */
-    protected void duplicateMetadata(String spaceId,
-                                     String contentId,
-                                     Map<String, String> sourceMetadata) {
+    protected void duplicateProperties(String spaceId, String contentId,
+                                       Map<String, String> sourceProperties) {
         // TODO implement
     }
 
@@ -143,18 +154,31 @@ public class DuplicationTaskProcessor implements TaskProcessor {
 
     protected void failTask(String message)
         throws DuplicationTaskExecutionFailedException {
-        throw new DuplicationTaskExecutionFailedException(buildMessage(message));
+        throw new DuplicationTaskExecutionFailedException(
+            buildFailureMessage(message));
     }
 
     protected void failTask(String message, Throwable cause)
         throws DuplicationTaskExecutionFailedException {
-        throw new DuplicationTaskExecutionFailedException(buildMessage(message),
-                                                          cause);
+        throw new DuplicationTaskExecutionFailedException(
+            buildFailureMessage(message), cause);
     }
 
-    private String buildMessage(String message) {
-        // TODO build message using task
-        return message;
+    private String buildFailureMessage(String message) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Failure to duplicate content item due to:");
+        builder.append(message);
+        builder.append(" Account: ");
+        builder.append(task.getAccount());
+        builder.append(" Source StoreID: ");
+        builder.append(task.getSourceStoreId());
+        builder.append(" Destination StoreID: ");
+        builder.append(task.getDestStoreId());
+        builder.append(" SpaceID: ");
+        builder.append(task.getSpaceId());
+        builder.append(" ContentID: ");
+        builder.append(task.getContentId());
+        return builder.toString();
     }
 
 }
