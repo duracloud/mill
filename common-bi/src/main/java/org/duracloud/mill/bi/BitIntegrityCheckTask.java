0e6430f43459245abdd67cbad360486db8ff1d8a
Daniel Bernstein <dabylon@gmail.com>
2014-04-22 10:36:38 -0700

    Initial commit to support https://jira.duraspace.org/browse/DPS-62 as well as other jiras depending on BitIntegrityCheckTask.  This commit includes the initial implementation of BitIntegrityCheckTask which is located in the new common-bi module.

---
+++ common-bi/src/main/java/org/duracloud/mill/bi/BitIntegrityCheckTask.java
@@ -0,0 +1,29 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
+package org.duracloud.mill.bi;
+
+import org.duracloud.common.queue.task.Task;
+import org.duracloud.common.queue.task.TypedTask;
+
+/**
+ * Provides the information necessary to complete a bit integrity check on a
+ * content item.
+ * 
+ * @author Daniel Bernstein 
+ *         Date: 04/22/2014
+ */
+public class BitIntegrityCheckTask extends TypedTask {
+
+    @Override
+    public Task writeTask() {
+        Task task = super.writeTask();
+        task.setType(Task.Type.BIT);
+        return task;
+    }
+ 
+}
