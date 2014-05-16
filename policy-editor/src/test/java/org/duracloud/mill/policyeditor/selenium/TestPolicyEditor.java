79c185a029c9fc7f10241f069121917958920b1b
Daniel Bernstein <dabylon@gmail.com>
2014-05-16 11:25:21 -0700

    Adds in creation of test duplication policies before running selenium tests for the policy editor. A preconfigured duplication policy file is not required.

--- policy-editor/src/test/java/org/duracloud/mill/policyeditor/selenium/TestPolicyEditor.java
+++ policy-editor/src/test/java/org/duracloud/mill/policyeditor/selenium/TestPolicyEditor.java
@@ -7,19 +7,74 @@
  */
 package org.duracloud.mill.policyeditor.selenium;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
+import org.duracloud.mill.dup.repo.DuplicationPolicyRepo;
+import org.duracloud.mill.dup.repo.S3DuplicationPolicyRepo;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+
 /**
- * @author Daniel Bernstein
- *	       Date: Dec 2, 2013
+ * @author Daniel Bernstein Date: Dec 2, 2013
  */
 public class TestPolicyEditor extends BaseSeleniumTest {
     private static final String LOGIN_LOCATOR = "css=#loginButton";
+
+    private static AmazonS3Client      s3Client;
+    private static String              bucketName;
+ 
     /**
      * @throws java.lang.Exception
      */
+    
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        File policyAccountsFile = new File(
+                "src/test/resources/duplication-accounts.json");
+        File policyFile = new File("src/test/resources/duplication-policy.json");
+
+        assertTrue(policyAccountsFile.exists());
+        assertTrue(policyFile.exists());
+        s3Client = new AmazonS3Client();
+
+        // Create policy bucket
+        String accessKey = System.getenv("AWS_ACCESS_KEY_ID");
+        bucketName = accessKey.toLowerCase()  + "." +
+            S3DuplicationPolicyRepo.DUP_POLICY_REPO_BUCKET_SUFFIX;
+        s3Client.createBucket(bucketName);
+
+        // Load accounts list
+        s3Client.putObject(bucketName,
+                           DuplicationPolicyRepo.DUP_ACCOUNTS_NAME,
+                           policyAccountsFile);
+
+        // Load policies
+        String acct1PolicyName =
+            "account1" + DuplicationPolicyRepo.DUP_POLICY_SUFFIX;
+        s3Client.putObject(bucketName, acct1PolicyName, policyFile);
+    }
+    
+    @AfterClass
+    public static void afterClass(){
+        // Clear policy bucket contents
+        for(S3ObjectSummary object :
+            s3Client.listObjects(bucketName).getObjectSummaries()) {
+            s3Client.deleteObject(bucketName, object.getKey());
+        }
+
+        // Remove policy bucket
+        s3Client.deleteBucket(bucketName);
+
+    }
+    
+    @Before
     public void setUp() throws Exception {
         super.before();
     }
@@ -45,9 +100,9 @@ public class TestPolicyEditor extends BaseSeleniumTest {
     private void clickLoginButton() {
         assertTrue(sc.isVisible(LOGIN_LOCATOR));
         sc.click(LOGIN_LOCATOR);
-        sleep(3000);
+        sleep(4000);
     }
-    
+
     @Test
     public void testAccounts() {
         openAccounts();
@@ -63,7 +118,7 @@ public class TestPolicyEditor extends BaseSeleniumTest {
         typeField("password");
         typeField("subdomain");
         clickLoginButton();
-        sleep(3000);
+        sleep(4000);
     }
 
     /**
