b7597472f4a4b6001ea3da6253cb50a6a6f3d320
Daniel Bernstein <dabylon@gmail.com>
2014-07-08 10:53:20 -0700

    Removes hardcoded database credentials.

--- credentialsrepo-impl/src/main/java/org/duracloud/mill/credentials/impl/CredentialsRepoLocator.java
+++ credentialsrepo-impl/src/main/java/org/duracloud/mill/credentials/impl/CredentialsRepoLocator.java
@@ -1,15 +1,21 @@
+/*
+ * The contents of this file are subject to the license and copyright
+ * detailed in the LICENSE and NOTICE files at the root of the source
+ * tree and available online at
+ *
+ *     http://duracloud.org/license/
+ */
 package org.duracloud.mill.credentials.impl;
 
 import org.duracloud.mill.credentials.CredentialsRepo;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+/**
+ * 
+ * @author Daniel Bernstein
+ *
+ */
 public class CredentialsRepoLocator {
     public static CredentialsRepo get(){
-        System.setProperty("db.user", "ama");
-        System.setProperty("db.pass", "ama");
-        System.setProperty("db.name", "ama");
-        System.setProperty("db.host", "localhost");
-        System.setProperty("db.port", "3306");
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("jpa-config.xml");
         return ctx.getBean(CredentialsRepo.class);
 
