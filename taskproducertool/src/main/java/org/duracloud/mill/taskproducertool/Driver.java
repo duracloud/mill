75b1c6d876574e607b81576cb7deec7b0a287086
Daniel Bernstein <dabylon@gmail.com>
2015-04-06 15:15:05 -0700

    Adds ability to specify a file containing a list of content ids to the taskproducertool.

--- taskproducertool/src/main/java/org/duracloud/mill/taskproducertool/Driver.java
+++ taskproducertool/src/main/java/org/duracloud/mill/taskproducertool/Driver.java
@@ -7,6 +7,13 @@
  */
 package org.duracloud.mill.taskproducertool;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.GnuParser;
@@ -14,6 +21,7 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
 import org.duracloud.audit.task.AuditTask;
 import org.duracloud.audit.task.AuditTask.ActionType;
 import org.duracloud.common.queue.TaskQueue;
@@ -112,6 +120,12 @@ public class Driver {
         contentId.setArgName("contentId");
         options.addOption(contentId);
 
+        Option contentIdFile = new Option("f", "content-id-file", true,
+                "A file containing a list of content ids");
+        contentIdFile.setArgs(1);
+        contentIdFile.setArgName("content-id-file");
+        options.addOption(contentIdFile);
+
         return options;
     }
 
@@ -130,6 +144,34 @@ public class Driver {
             String spaceId = cmd.getOptionValue("d");
             String contentId = cmd.getOptionValue("c");
 
+            List<String> contentIds = new ArrayList<>();
+            
+            if(contentId != null){
+                contentIds.add(contentId);
+            }
+            
+            String contentIdFilePath = cmd.getOptionValue("f");
+            
+            if(contentIdFilePath != null){
+                File file = new File(contentIdFilePath);
+                if(!file.exists()){
+                    throw new IOException("contentIds file does not exist: " + file.getAbsolutePath());
+                }
+                
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+                String line = null;
+                while((line = reader.readLine()) != null){
+                    if(!StringUtils.isBlank(line)){
+                        contentIds.add(line.trim());
+                    }
+                }
+            }
+            
+            if(contentIds.size() == 0){
+                throw new Exception("You must specify at least one content id " +
+                		"either using the -c parameter or -f parameter.");
+            }
+            
             if(username != null){
                 System.setProperty("aws.accessKeyId", username);
             }
@@ -138,42 +180,37 @@ public class Driver {
                 System.setProperty("aws.secretKey", password);
             }
 
+
             TaskQueue taskQueue = new SQSTaskQueue(queue);
             
-            TypedTask typedTask;
-            
-            if(taskType.equals(Type.DUP)) {
-                DuplicationTask dupTask = new DuplicationTask();
-                dupTask.setAccount(subdomain);
-                dupTask.setStoreId(sourceStoreId);
-                dupTask.setSourceStoreId(sourceStoreId);
-                dupTask.setDestStoreId(destStoreId);
-                dupTask.setSpaceId(spaceId);
-                dupTask.setContentId(contentId);
-                typedTask = dupTask;
-            } else if(taskType.equals(Type.NOOP)) {
-                NoopTask noopTask = new NoopTask();
-                noopTask.setAccount(subdomain);
-                noopTask.setSpaceId(spaceId);
-                noopTask.setContentId(contentId);
-                noopTask.setStoreId(sourceStoreId);
-                typedTask = noopTask;
-            } else if(taskType.equals(Type.AUDIT)) {
-                AuditTask auditTask = new AuditTask();
-                auditTask.setAccount(subdomain);
-                auditTask.setSpaceId(spaceId);
-                auditTask.setContentId(contentId);
-                auditTask.setStoreId(sourceStoreId);
-                auditTask.setContentChecksum("checksum");
-                auditTask.setAction(ActionType.ADD_CONTENT.name());
-                auditTask.setContentSize("1000");
-                auditTask.setContentMimetype("text/plain");
-                typedTask = auditTask;
-            } else {
-               throw new RuntimeException("taskType " + taskType + " not supported.");
+            for(String content : contentIds){
+                TypedTask typedTask;
+                if(taskType.equals(Type.DUP)) {
+                    DuplicationTask dupTask = new DuplicationTask();
+                    dupTask.setSourceStoreId(sourceStoreId);
+                    dupTask.setDestStoreId(destStoreId);
+                    typedTask = dupTask;
+                } else if(taskType.equals(Type.NOOP)) {
+                    NoopTask noopTask = new NoopTask();
+                    typedTask = noopTask;
+                } else if(taskType.equals(Type.AUDIT)) {
+                    AuditTask auditTask = new AuditTask();
+                    auditTask.setContentChecksum("checksum");
+                    auditTask.setAction(ActionType.ADD_CONTENT.name());
+                    auditTask.setContentSize("0");
+                    auditTask.setContentMimetype("application/octet-stream");
+                    typedTask = auditTask;
+                } else {
+                   throw new RuntimeException("taskType " + taskType + " not supported.");
+                }
+
+                typedTask.setAccount(subdomain);
+                typedTask.setSpaceId(spaceId);
+                typedTask.setContentId(content);
+                typedTask.setStoreId(sourceStoreId);
+                taskQueue.put(typedTask.writeTask());
             }
-            
-            taskQueue.put(typedTask.writeTask());
+
             System.exit(0);
         } catch (Exception e) {
             e.printStackTrace();
