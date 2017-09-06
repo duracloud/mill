8867ce94a844fd4ca461b36fadb0b99a4c78475f
Daniel Bernstein <dabylon@gmail.com>
2017-09-05 21:38:33 -0700

    Removes obselete schema changes and adds in updated mill ddl schema.

--- resources/schema-update.2.sql
+++
@@ -1,5 +0,0 @@
-alter table manifest_item  modify modified datetime(3) not null;
-alter table audit_log_item modify modified datetime(3) not null;
-alter table bit_report modify modified datetime(3) not null;
-alter table bit_log_item  modify modified datetime(3) not null;
-
--- resources/schema-update.3.sql
+++
@@ -1,6 +0,0 @@
-alter table manifest_item  add column version int not null default 1;
-alter table audit_log_item add column version int not null default 1;
-alter table bit_report add column version int not null default 1;
-alter table bit_log_item  add column version int not null default 1;
-alter table space_stats add column version int not null default 1;
-
