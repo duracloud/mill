c73ab22df2ba314365180e32de9ef1835b4a2321
dbernstein <dabylon@gmail.com>
2021-11-03 11:31:53 -0700

    Update worker scripts.

--- docker/scripts/bit-worker.sh
+++ docker/scripts/bit-worker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-
-#TODO implement script
+echo "running workman";
+# execute workman
+java -Dlog.level=$LOG_LEVEL -Dduracloud.home=$MILL_HOME -jar /opt/app/workman-${MILL_VERSION}.jar -c "${CONFIG_FILE_PATH}" -q "queue.name.bit-integrity,queue.name.audit,queue.name.dup-high-priority,queue.name.dup-low-priority";
---
+++ docker/scripts/storage-stats-worker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "running workman";
+# execute workman
+java -Dlog.level=$LOG_LEVEL -Dduracloud.home=$MILL_HOME -jar /opt/app/workman-${MILL_VERSION}.jar -c "${CONFIG_FILE_PATH}" -q "queue.name.storagestats";
