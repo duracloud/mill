ebd350c1ff8ef93c6a5030c8726b29c8b5a61d30
Daniel Bernstein <dabylon@gmail.com>
2023-05-05 19:17:16 -0700

    Remove config of sumo sources path from user properties.

--- docker/scripts/setup.sh
+++ docker/scripts/setup.sh
@@ -6,11 +6,8 @@ printenv | grep -v "no_proxy" >> /etc/environment
 sh -xc "echo ${HOST_NAME} > /etc/hostname; hostname -F /etc/hostname"
 sh -xc "sed -i -e '/^127.0.1.1/d' /etc/hosts; echo 127.0.1.1 ${HOST_NAME}.${DOMAIN} ${HOST_NAME} >> /etc/hosts"
 
-
 # configure sumo
 cp $MILL_HOME/sumo.conf /opt/SumoCollector/config/user.properties
-
-echo "sources=/opt/app/sumo-sources.json" >> /opt/SumoCollector/config/user.properties
 
 # start sumo
 service collector start
